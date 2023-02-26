1286e14ce6cf35dab477d94f447493c7c8b3e00c
Anh Tran <trantanhdev@gmail.com>
2023-02-25 19:46:05 -0600

    feat: add livereload

---
+++ livereload_server.py
@@ -0,0 +1,14 @@
+from livereload import Server
+from src import create_app
+
+app = create_app()
+app.debug = True
+
+def main():
+  print("Start server with livereload")
+  server = Server(app.wsgi_app)
+  server.watch('src/templates/*.html.jinja', 'echo Template is changed.')
+  server.serve()
+
+if __name__ == "__main__":
+  main()
